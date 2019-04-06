7806dcff9705762bf75ef9616c462b090c54355c
soundarya2 <saryasweet@gmail.com>
2019-04-06 20:22:30 +0530

    uploading a file

---
+++ com/siriuscom/ipl_scorer/bundle/dataproviders/TestComponent.java
@@ -0,0 +1,8 @@
+package com.siriuscom.ipl_scorer.bundle.dataproviders;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(immediate=true,name="Test Process")
+public class TestComponent {
+
+}
